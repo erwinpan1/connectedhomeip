ba1413e082f9624d91b1aca3a92d71e4081fb056
Boris Zbarsky <bzbarsky@apple.com>
2024-07-16 13:48:33 -0400

    Stop importing operator* and operator-> into Nullable. (#34353)
    These are serious footguns because:
    
    1.  They don't do checks that there is actually a value, and will silently
        produce garbage.
    2.  They are really easy to accidentally use without a IsNull() check
        (especially operator->).
    
    I've now reviewed several PRs where people were misusing these and helped a few
    other people who were misusing them and then not getting expected behavior, so
    it would be better if we just didn't have these at all.  Nullable is not a
    pointer and people shouldn't treat it like one.

--- src/app/cluster-building-blocks/tests/TestQuieterReporting.cpp
+++ src/app/cluster-building-blocks/tests/TestQuieterReporting.cpp
@@ -97,7 +97,7 @@ TEST(TestQuieterReporting, ChangeOnIncrementPolicyWorks)
     QuieterReportingAttribute<int> attribute{ MakeNullable<int>(10) };
 
     // Always start not dirty (because first sub priming always just read value anyway).
-    ASSERT_EQ(*attribute.value(), 10);
+    ASSERT_EQ(attribute.value().Value(), 10);
 
     auto now = fakeClock.now();
 
@@ -148,7 +148,7 @@ TEST(TestQuieterReporting, ChangeOnDecrementPolicyWorks)
     QuieterReportingAttribute<int> attribute{ MakeNullable<int>(9) };
 
     // Always start not dirty (because first sub priming always just read value anyway).
-    ASSERT_EQ(*attribute.value(), 9);
+    ASSERT_EQ(attribute.value().Value(), 9);
 
     auto now = fakeClock.now();
 
@@ -201,7 +201,7 @@ TEST(TestQuieterReporting, SufficientChangePredicateWorks)
     QuieterReportingAttribute<int> attribute{ MakeNullable<int>(9) };
 
     // Always start not dirty (because first sub priming always just read value anyway).
-    ASSERT_EQ(*attribute.value(), 9);
+    ASSERT_EQ(attribute.value().Value(), 9);
 
     auto now = fakeClock.now();
 
